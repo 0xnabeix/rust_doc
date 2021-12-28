ad60f9d8dd660f800f095a05db764dd316dffbd7
torutona <sololouve@gmail.com>
2021-12-28 11:33:33 -0800

    match

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -146,12 +146,55 @@
 // }
 
 
-fn main() {
-    let num = Some(5);
+// fn main() {
+//     let num = Some(5);
 
-    match num {
-        Some(x) if x < 5 => println!("less than five: {}", x),
-        Some(x) => println!("{}", x),
-        None => (),
+//     match num {
+//         Some(x) if x < 5 => println!("less than five: {}", x),
+//         Some(x) => println!("{}", x),
+//         None => (),
+//     }
+// }
+
+
+// fn main() {
+    // let x = Some(5);
+    // let y = 5;
+
+    // match x {
+    //     Some(50) => println!("Got 50"),
+    //     Some(n) if n == y => println!("Matched, n = {}", n),
+    //     _ => println!("Default case, x = {:?}", x),
+    // }
+
+    // println!("at the end: x = {:?}, y = {}", x, y);
+
+//     let x = 4;
+//     let y = false;
+
+//     match x {
+//         4 | 5 | 6 if y => println!("yes"),
+//         _ => println!("no"),
+//     }
+// }
+
+
+
+fn main() {
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello {id: 10};
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
     }
 }
+

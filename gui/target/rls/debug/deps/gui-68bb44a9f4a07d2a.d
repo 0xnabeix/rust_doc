b125af2e71ddae02bc31d91647c2936347d23cca
torutona <sololouve@gmail.com>
2021-12-28 06:42:55 -0800

    object oriented mathcing

---
+++ gui/target/rls/debug/deps/gui-68bb44a9f4a07d2a.d
@@ -0,0 +1,5 @@
+/home/ruster/Documents/rust_doc/gui/target/rls/debug/deps/gui-68bb44a9f4a07d2a.rmeta: src/main.rs
+
+/home/ruster/Documents/rust_doc/gui/target/rls/debug/deps/gui-68bb44a9f4a07d2a.d: src/main.rs
+
+src/main.rs:
---
+++ gui/target/rls/debug/deps/gui-f7b7ae4b25e0ca8d.d
@@ -0,0 +1,5 @@
+/home/ruster/Documents/rust_doc/gui/target/rls/debug/deps/gui-f7b7ae4b25e0ca8d.rmeta: src/main.rs
+
+/home/ruster/Documents/rust_doc/gui/target/rls/debug/deps/gui-f7b7ae4b25e0ca8d.d: src/main.rs
+
+src/main.rs:

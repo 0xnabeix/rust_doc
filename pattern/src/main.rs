bc1be06a95a990cf9bb8136ea4eb376c827bef64
torutona <sololouve@gmail.com>
2021-12-28 09:57:05 -0800

    destructuring struct

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -41,10 +41,21 @@ struct Point {
     y: i32,
 }
 
-fn main() {
-    let p = Point {x: 0, y: 7};
+// fn main() {
+//     let p = Point {x: 0, y: 7};
 
-    let Point{ x: a, y: b } = p;
-    assert_eq!(0, a);
-    assert_eq!(7, b);
+//     let Point{ x: a, y: b } = p;
+//     assert_eq!(0, a);
+//     assert_eq!(7, b);
+// }
+
+
+fn main() {
+    let p = Point { x: 1, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neithger axis: ({}, {})", x, y),
+    }
 }

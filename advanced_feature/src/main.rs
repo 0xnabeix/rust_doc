009a09384a6fc2ff6349bae91fa0c48de9d4e1a5
torutona <sololouve@gmail.com>
2021-12-28 12:49:53 -0800

    unsafe rust

--- advanced_feature/src/main.rs
+++ advanced_feature/src/main.rs
@@ -1,19 +1,19 @@
-fn split_at_mut(slice: &mut[i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+use std::slice;
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     let len = slice.len();
+    let ptr = slice.as_mut_ptr();
 
     assert!(mid <= len);
 
-    (&mut slice[..mid], &mut slice[mid..])
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
 
 fn main() {
-    let mut num = 5;
-    
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
 
-    unsafe {
-        println!("r1 is: {}", *r1);
-        println!("r2 is: {}", *r2);
-    }
 }

ac73bcd3197618800d297559f837909c0682327f
torutona <sololouve@gmail.com>
2021-12-28 10:32:23 -0800

    ignoring 2

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -111,10 +111,27 @@ struct Point {
 // }
 
 
-fn foo(_: i32, y: i32) {
-    println!("This code only uses the y parameter: {}", y);
-}
+// fn foo(_: i32, y: i32) {
+//     println!("This code only uses the y parameter: {}", y);
+// }
+
+// fn main() {
+//     foo(3, 5);
+// }
+
 
 fn main() {
-    foo(3, 5);
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match(setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("Setting is {:?}", setting_value);
 }

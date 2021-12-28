abdce372abef6bb48641de877229b01ad7cf765d
torutona <sololouve@gmail.com>
2021-12-28 10:19:13 -0800

    change the color

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -60,24 +60,52 @@ struct Point {
 //     }
 // }
 
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+// fn main() {
+//     let msg = Message::ChangeColor(0, 160, 255);
+
+//     let msg = Message::ChangeColor(0, 160, 255);
+
+//     match msg {
+//         Message::ChangeColor(r, g, b) => {
+//             println!("Change the color to red {}, green {} and blue {}", r, g, b);
+//         },
+//         _ => {
+//             println!("Welcome");
+//         }
+//     }
+// }
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
 fn main() {
-    let msg = Message::ChangeColor(0, 160, 255);
-
-    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::ChangeColor(r, g, b) => {
-            println!("Change the color to red {}, green {} and blue {}", r, g, b);
-        },
-        _ => {
-            println!("Welcome");
-        }
+        Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
+            "Change the color to red {}, green {}, blue {}",
+            r, g, b
+        ),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
     }
 }

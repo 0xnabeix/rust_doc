de4810a3c0f463309909a4ad1faaa248853f35c2
torutona <sololouve@gmail.com>
2021-12-28 10:25:43 -0800

    ignorig 1

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -82,30 +82,39 @@ struct Point {
 //     }
 // }
 
-enum Color {
-    Rgb(i32, i32, i32),
-    Hsv(i32, i32, i32),
-}
+// enum Color {
+//     Rgb(i32, i32, i32),
+//     Hsv(i32, i32, i32),
+// }
 
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(Color),
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(Color),
+// }
+
+// fn main() {
+//     let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+//     match msg {
+//         Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
+//             "Change the color to red {}, green {}, blue {}",
+//             r, g, b
+//         ),
+//         Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+//             "Change the color to hue {}, saturation {}, and value {}",
+//             h, s, v
+//         ),
+//         _ => (),
+//     }
+// }
+
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
 
 fn main() {
-    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
-
-    match msg {
-        Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
-            "Change the color to red {}, green {}, blue {}",
-            r, g, b
-        ),
-        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
-            "Change the color to hue {}, saturation {}, and value {}",
-            h, s, v
-        ),
-        _ => (),
-    }
+    foo(3, 5);
 }

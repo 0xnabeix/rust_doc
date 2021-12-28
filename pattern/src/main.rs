f09692c07359270e68fd43c560537ad9e0ae680e
torutona <sololouve@gmail.com>
2021-12-28 08:16:24 -0800

    mathcing arm

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -10,10 +10,25 @@
 //     }
 // }
 
-fn main() {
-    let v = vec!['a', 'b', 'c'];
+// fn main() {
+//     let v = vec!['a', 'b', 'c'];
 
-    for item in v.iter() {
-        println!("item is {}", item);
+//     for item in v.iter() {
+//         println!("item is {}", item);
+//     }
+// }
+
+
+
+fn main() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
     }
+
+    println!("at the end: x= {:?}, y = {:?}", x, y);
 }

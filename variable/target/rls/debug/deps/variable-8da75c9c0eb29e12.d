178189ee48feae1e812ef2c978c606264dfb7067
torutona <sololouve@gmail.com>
2022-01-07 11:57:26 -0800

    shadowing varialbe

---
+++ variable/target/rls/debug/deps/variable-8da75c9c0eb29e12.d
@@ -0,0 +1,5 @@
+/home/ruster/Documents/rust_doc/variable/target/rls/debug/deps/variable-8da75c9c0eb29e12.rmeta: src/main.rs
+
+/home/ruster/Documents/rust_doc/variable/target/rls/debug/deps/variable-8da75c9c0eb29e12.d: src/main.rs
+
+src/main.rs:
